94c360e06cb31e3db5b4fa10474d673f2da966f3
gautamkumar121989 <gautamkumar121989@gmail.com>
2018-01-18 14:23:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var $ :any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  groceryList = [];
+  groceryItem = '';
+
+  addGrocery(){
+    this.groceryList.push(this.groceryItem);
+  }
+
+  openModal(){
+    $("#myModal").modal('show');
+  }
 }
